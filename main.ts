887fc16a3a5b9078c55d2956c94869454bdc7b91
rien7 <zqrren@outlook.com>
2022-11-03 22:15:55 +0800

    match tag with data

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Modal, Plugin, PluginSettingTab, setIcon, Setting, TextComponent } from 'obsidian';
+import { App, CachedMetadata, Modal, Plugin, PluginSettingTab, setIcon, Setting, TextComponent, TFile } from 'obsidian';
 
 interface ColorfulTagSetting {
 	firstTime: boolean;
@@ -8,6 +8,7 @@ interface ColorfulTagSetting {
 	defaultStyle: Map<string, any>;
 	defaultGlobal: Map<string, boolean>;
 	styleList: Array<Object>;
+	// {filename:{file_size:0,tag_num:0,#tag-offset:{_i:idx,_l:line,_c:col,item:any,...}},...}
 	tagDetail: Object;
 }
 
@@ -24,89 +25,69 @@ const DEFAULT_SETTINGS: ColorfulTagSetting = {
 
 export default class ColorfulTag extends Plugin {
 	settings: ColorfulTagSetting;
+	fs: Map<number, (e: Event) => void> = new Map();
+	timer: NodeJS.Timer;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new ColorfulTagSettingTab(this.app, this));
-		this.addListenerForFileOpen();
+		this.app.workspace.on("active-leaf-change", async () => {
+			await this.addListenerForTag();
+		});
+		this.app.metadataCache.on("changed", async (file, data, cache) => {
+			await this.handleFileChange(file, data, cache);
+		});
 	}
 
 	onunload() {
 		// remove listener
-		this.app.workspace.off("active-leaf-change", this.addListenerForTag);
+		this.app.workspace.off("active-leaf-change", async () => {
+			await this.addListenerForTag();
+		});
+		this.app.metadataCache.off("changed", async (file, data, cache) => {
+			await this.handleFileChange(file, data, cache);
+		});
+		this.removeListener();
 	}
 
-	addPopupCss(tag: string) {
-		let head = document.querySelector("head")!;
-		let del = head.querySelectorAll("[colorful-tag-plugin-popup]");
-		del.forEach((d) => { d.remove() });
-		let el = head.createEl("style", { "type": "text/css", "attr": { "colorful-tag-plugin-popup": "" } });
-		let styles = this.settings.styleList;
-		let global = new Map(Object.entries(this.settings.global));
-		let style = new Map<string, any>();
-		tag = tag.substring(1);
-		for (let i in styles) {
-			let m = new Map(Object.entries(styles[i]));
-			if (!m.get("enable")) continue;
-			if (m.get("tag") == tag) {
-				style = m;
-				break;
-			}
-		}
-		let tag_lp = tag;
-		// replace / to \/, because / is a special character in css
-		tag = tag.replace(/\//g, "\\/");
-		tag_lp = tag_lp.replace(/\//g, "");
-		let background_color = style.get("background-color") || global.get("background-color") || "";
-		let text_color = style.get("text-color") || global.get("text-color") || "";
-		let prefix = style.get("prefix") || global.get("prefix") || "";
-		let suffix = style.get("suffix") || global.get("suffix") || "";
-		let radius = style.get("radius") || global.get("radius") || "";
-		let text_size = style.get("text-size") || global.get("text-size") || "";
-		let font_weight = style.get("font-weight") || global.get("font-weight") || "";
-		let css = "";
-		css += `#colorful-tag-popup { position: absolute; z-index: 100; }`;
-		css += `.colorful-tag-popup-header { display: flex; padding: 5px 10px; background-color: ${background_color}; color: ${text_color}; font-size: ${text_size}; font-weight: ${font_weight}; border-radius: 10px 10px 0 0; }`;
-		css += `.colorful-tag-popup-pin { margin-left: auto; }`;
-		css += `.colorful-tag-popup-pin.pinned > svg { transform: rotate(45deg); }`;
-		if (prefix != "") {
-			css += `.colorful-tag-popup-header:before { content: "${prefix}"; }`;
-		}
-		if (suffix != "") {
-			css += `.colorful-tag-popup-header:after { content: "${suffix}"; }`;
-		}
-		css += `.colorful-tag-popup-body { padding: 5px 10px; border: 4px solid ${background_color}; border-radius: 0 0 10px 10px; border-top: none; background-color: #fff; }`;
-		css += `#colorful-tag-popup input[type="text"] { border: none; }`;
-		css += `#colorful-tag-popup input[type="text"]:focus { border: inherit; }`;
-		el.setText(css);
+	async addListenerForTag() {
+		let _file = this.app.workspace.getActiveFile();
+		if (_file == null) return;
+		let file = _file!;
+		let cache = this.app.metadataCache.getFileCache(file);
+		if (cache?.tags == null) return;
+		this.removeListener();
+		// query tag on this page
+		let tags_dom = document.querySelectorAll(".cm-hashtag.cm-hashtag-end");
+		tags_dom.forEach((tag, i) => {
+			//get previous element
+			let prev = tag.previousElementSibling!;
+			let f = (e: Event) => { this.listener(e, i) };
+			tag.addEventListener("mouseover", f);
+			prev.addEventListener("mouseover", f);
+			this.fs.set(i, f);
+		});
 	}
 
-	async refreshPopupBody(body: Setting, tag: string, filename: string, pos: string) {
-		let tagDetail = new Map(Object.entries(this.settings.tagDetail));
-		let tags = new Map<string, any>(Object.entries(tagDetail.get(filename)));
-		let items = new Map(Object.entries(tags.get(`${tag}-${pos}`)));
-		for (let [k, v] of items.entries()) {
-			let item = body.addText((t) => {
-				t.setValue(v as string);
-				t.onChange(async (value) => {
-					items.set(k, value);
-					tags.set(`${tag}-${pos}`, Object.fromEntries(items));
-					tagDetail.set(filename, Object.fromEntries(tags));
-					this.settings.tagDetail = Object.fromEntries(tagDetail);
-					await this.saveData(this.settings);
-				});
-			});
-			item.setName(k);
-		}
-	}
-
-	async addPopup(event: MouseEvent, tag: string, filename: string, pos: string) {
+	async addPopup(event: MouseEvent, i: number) {
 		// get mouse position
 		let countdown = 3;
 		let x = event.clientX;
 		let y = event.clientY;
+
+		// get cur file
+		let curFile = this.app.workspace.getActiveFile()!;
+		// get cache
+		let cache = this.app.metadataCache.getFileCache(curFile)!;
+		// get tags
+		let tag = cache.tags![i].tag;
+		// get tag position
+		let pos = cache.tags![i].position.start.offset.toString();
+		let line = cache.tags![i].position.start.line;
+		let col = cache.tags![i].position.start.col;
+		let filename = curFile.path;
 		
 		let popup = document.createElement("div");
 		this.addPopupCss(tag);
@@ -137,7 +118,7 @@ export default class ColorfulTag extends Plugin {
 			body.addButton((b) => {
 				b.setButtonText("Add");
 				b.onClick(async () => {
-					tagDetail.set(filename, Object.fromEntries(new Map([[`${tag}-${pos}`, Object.fromEntries(new Map([["test", "testtest"]]))]])));
+					tagDetail.set(filename, Object.fromEntries(new Map([[`${tag}-${pos}`, Object.fromEntries(new Map<string, any>([["_i", i], ["_l", line], ["_c", col], ["test", "testtest"]]))]])));
 					this.settings.tagDetail = Object.fromEntries(tagDetail);
 					this.saveSettings();
 					await this.saveData(this.settings);
@@ -152,8 +133,8 @@ export default class ColorfulTag extends Plugin {
 				body.addButton((b) => {
 					b.setButtonText("Add");
 					b.onClick(async () => {
-						_tag.set(`${tag}-${pos}`, Object.fromEntries(new Map([["test", "testtest"]])));
-						tagDetail.set(filename, Object.fromEntries(_file));
+						_tag.set(`${tag}-${pos}`, Object.fromEntries(new Map<string, any>([["_i", i], ["_l", line], ["_c", col], ["test", "testtest"]])));
+						tagDetail.set(filename, Object.fromEntries(_tag));
 						this.settings.tagDetail = Object.fromEntries(tagDetail);
 						await this.saveData(this.settings);
 						body.clear();
@@ -177,46 +158,196 @@ export default class ColorfulTag extends Plugin {
 		let timer = setInterval(() => {
 			countdown--;
 			if (countdown == 0) {
-				popup.remove();
+				if (popup != null) {
+					popup.remove();
+				}
 				clearInterval(timer);
 			}
 		}, 1000);
+		this.timer = timer;
 	}
 
-	async addListenerForTag() {
-		let _file = this.app.workspace.getActiveFile();
-		if (_file == null) return;
-		let file = _file!;
-		let cache = this.app.metadataCache.getFileCache(file);
-		if (cache?.tags == null) return;
-		// query tag on this page
-		let tags = document.querySelectorAll(".cm-hashtag.cm-hashtag-end");
-		let i = 0;
-		tags.forEach((tag) => {
-			//get previous element
-			let pos = cache?.tags![i].position;
-			let tagname = cache?.tags![i].tag!;
-			let prev = tag.previousElementSibling!;
-			this.registerDomEvent(tag as HTMLElement, "mouseover", (e) => {
-				// check if popup exists
-				let popup = document.querySelector("#colorful-tag-popup");
-				if (popup != null)
-					return
-				this.addPopup(e as MouseEvent, tagname, file.path, pos!.start.line + ":" + pos!.start.col);
-			});
-			this.registerDomEvent(prev as HTMLElement, "mouseover", (e) => {
-				let popup = document.querySelector("#colorful-tag-popup");
-				if (popup != null)
-					return
-				this.addPopup(e as MouseEvent, tagname, file.path, pos!.start.line + ":" + pos!.start.col);
-			});
-			i++;
-		});
+	addPopupCss(tag: string) {
+		let head = document.querySelector("head")!;
+		let del = head.querySelectorAll("[colorful-tag-plugin-popup]");
+		del.forEach((d) => { d.remove() });
+		let el = head.createEl("style", { "type": "text/css", "attr": { "colorful-tag-plugin-popup": "" } });
+		let styles = this.settings.styleList;
+		let global = new Map(Object.entries(this.settings.global));
+		let style = new Map<string, any>();
+		tag = tag.substring(1);
+		for (let i in styles) {
+			let m = new Map(Object.entries(styles[i]));
+			if (!m.get("enable")) continue;
+			if (m.get("tag") == tag) {
+				style = m;
+				break;
+			}
+		}
+		let tag_lp = tag;
+		// replace / to \/, because / is a special character in css
+		tag = tag.replace(/\//g, "\\/");
+		tag_lp = tag_lp.replace(/\//g, "");
+		let background_color = style.get("background-color") || global.get("background-color") || "";
+		let text_color = style.get("text-color") || global.get("text-color") || "";
+		let prefix = style.get("prefix") || global.get("prefix") || "";
+		let suffix = style.get("suffix") || global.get("suffix") || "";
+		let text_size = style.get("text-size") || global.get("text-size") || "";
+		let font_weight = style.get("font-weight") || global.get("font-weight") || "";
+		let css = "";
+		css += `#colorful-tag-popup { position: absolute; z-index: 100; }`;
+		css += `.colorful-tag-popup-header { display: flex; padding: 5px 10px; background-color: ${background_color}; color: ${text_color}; font-size: ${text_size}; font-weight: ${font_weight}; border-radius: 10px 10px 0 0; }`;
+		css += `.colorful-tag-popup-pin { margin-left: auto; }`;
+		css += `.colorful-tag-popup-pin.pinned > svg { transform: rotate(45deg); }`;
+		if (prefix != "") {
+			css += `.colorful-tag-popup-header:before { content: "${prefix}"; }`;
+		}
+		if (suffix != "") {
+			css += `.colorful-tag-popup-header:after { content: "${suffix}"; }`;
+		}
+		css += `.colorful-tag-popup-body { padding: 5px 10px; border: 4px solid ${background_color}; border-radius: 0 0 10px 10px; border-top: none; background-color: #fff; }`;
+		css += `#colorful-tag-popup input[type="text"] { border: none; }`;
+		css += `#colorful-tag-popup input[type="text"]:focus { border: inherit; }`;
+		el.setText(css);
 	}
-	async addListenerForFileOpen() {
-		this.app.workspace.on("active-leaf-change", async () => {
-			await this.addListenerForTag();
+
+	async refreshPopupBody(body: Setting, tag: string, filename: string, pos: string) {
+		let tagDetail = new Map(Object.entries(this.settings.tagDetail));
+		let tags = new Map<string, any>(Object.entries(tagDetail.get(filename)));
+		let items = new Map(Object.entries(tags.get(`${tag}-${pos}`)));
+		for (let [k, v] of items.entries()) {
+			if (k.charAt(0) == "_") continue;
+			let item = body.addText((t) => {
+				t.setValue(v as string);
+				t.onChange(async (value) => {
+					items.set(k, value);
+					tags.set(`${tag}-${pos}`, Object.fromEntries(items));
+					tagDetail.set(filename, Object.fromEntries(tags));
+					this.settings.tagDetail = Object.fromEntries(tagDetail);
+					await this.saveData(this.settings);
+				});
+			});
+			item.setName(k);
+		}
+	}
+
+	async handleFileChange(file: TFile, data: string, cache: CachedMetadata) {
+		// ignore tag delete
+		let tags = cache.tags;
+		if (tags == undefined) { return; }
+		tags = tags!;
+		let tagDetail = new Map(Object.entries(this.settings.tagDetail));
+		let _file = tagDetail.get(file.path);
+		if (_file == undefined) { return };
+		let fileSize = data.length;
+		let _tags = new Map<string, any>(Object.entries(_file));
+		// console.log("=====start handle file change=====");
+		// console.log("tags_size: " + tags.length);
+		if (_tags.get("tag_num") == tags.length) {
+			let _new = new Map<string, any>();
+			for (let [k, v] of _tags.entries()) {
+				if (k.charAt(0) != "#") { continue; }
+				let _ori = new Map(Object.entries(v));
+				let _i = _ori.get("_i") as number;
+				let cur = tags[_i];
+				_ori.set("_l", cur.position.start.line);
+				_ori.set("_c", cur.position.start.col);
+				_new.set(`${cur.tag}-${cur.position.start.offset}`, Object.fromEntries(_ori));
+			}
+			_tags = _new;
+		} else {
+			this.removeListener();
+			let tags_dom = document.querySelectorAll(".cm-hashtag.cm-hashtag-end");
+			tags_dom.forEach((tag, i) => {
+				//get previous element
+				let prev = tag.previousElementSibling!;
+				let f = (e: Event) => { this.listener(e, i) };
+				tag.addEventListener("mouseover", f);
+				prev.addEventListener("mouseover", f);
+				this.fs.set(i, f);
+			});
+			let size_delta = fileSize - _tags.get("file_size");
+			// console.log("size offset: "+size_delta);
+			// console.log(_tags);
+			let temp = [];
+			for (let i = 0; i < tags.length; i++) {
+				let tag = tags[i];
+				let start = tag.position.start;
+				// tag: new info from cache
+				// _tags: old info from prev settings
+				
+				// ignore unchange tags
+				let ori = _tags.get(`${tag.tag}-${start.offset}`);
+				if (ori != undefined) {
+					let _ori = new Map(Object.entries(ori));
+					if (start.line == _ori.get("_l") && start.col == _ori.get("_c")) {
+						// console.log("unchange tag: "+ start.offset);
+						continue;
+					}
+				}
+				// change happen before tag
+				// console.log("start offset: "+start.offset);
+				// console.log(`${tag.tag}-${start.offset - size_delta}`)
+				ori = _tags.get(`${tag.tag}-${start.offset - size_delta}`)
+				if (ori != undefined) {
+					let _ori = new Map(Object.entries(ori));
+					if (start.line == _ori.get("_l") || start.col == _ori.get("_c")) {
+						_ori.set("_l", start.line);
+						_ori.set("_c", start.col);
+						_ori.set("_i", i);
+						let _replace = _tags.get(`${tag.tag}-${start.offset}`);
+						if (_replace != undefined) {
+							temp.push(_replace);
+						}
+						_tags.set(`${tag.tag}-${start.offset}`, Object.fromEntries(_ori));
+						_tags.delete(`${tag.tag}-${start.offset - size_delta}`);
+					} else {
+						for (let t = 0; t < temp.length; t++) {
+							let tmp = temp[t];
+							let _tmp = new Map(Object.entries(tmp));
+							if (start.line == _tmp.get("_l") || start.col == _tmp.get("_c")) {
+								// delete temp
+								temp.splice(t, 1);
+								_tmp.set("_l", start.line);
+								_tmp.set("_c", start.col);
+								_tmp.set("_i", i);
+								_tags.set(`${tag.tag}-${start.offset}`, Object.fromEntries(_tmp));
+								break;
+							}
+						}
+					}
+				} else {
+					// console.log("no match tag: "+start.offset);
+				}
+			}
+		}
+		// save
+		_tags.set("file_size", fileSize);
+		_tags.set("tag_num", tags.length);
+		tagDetail.set(file.path, Object.fromEntries(_tags));
+		this.settings.tagDetail = Object.fromEntries(tagDetail);
+		await this.saveData(this.settings);
+	}
+
+	listener(e: Event, i: number) {
+		let popup = document.querySelector("#colorful-tag-popup");
+		if (popup != null) {
+			clearInterval(this.timer);
+			popup.remove();
+		}
+		this.addPopup(e as MouseEvent, i);
+	}
+
+	removeListener() {
+		let tags_dom = document.querySelectorAll(".cm-hashtag.cm-hashtag-end");
+		tags_dom.forEach((tag, i) => {
+			for (let [i, f] of this.fs.entries()) {
+				let prev = tag.previousElementSibling!;
+				tag.removeEventListener("mouseover", f);
+				prev.removeEventListener("mouseover", f);
+			}
 		});
+		this.fs.clear();
 	}
 
 	async refresh() {
